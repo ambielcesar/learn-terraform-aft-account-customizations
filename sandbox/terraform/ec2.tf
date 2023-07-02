a6bbbc70fa76f0cec33558bb49db715c2bd4f80b
Cesar Ambiel <cambiel@amazon.com>
2023-07-02 14:04:01 -0300

    allowing ssh on ec2 instance

--- sandbox/terraform/ec2.tf
+++ sandbox/terraform/ec2.tf
@@ -24,3 +24,20 @@ resource "aws_instance" "CTLab-Machine" {
   }
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH on EC2 instance"
+  vpc_id      = var.ct_vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
--- sandbox/terraform/variables.tf
+++ sandbox/terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "subnet_private_id" {
   default = "subnet-06db4b27907f4bc66" # Orientações para copia da Subnet ID abaixo.
 }
+
+variable "ct_vpc_id" {
+  default = "vpc-0fee1e78e5ad2eb08" # Orientações para copia da VPC ID abaixo.
+}
